ff9ebefc340617d40ad63ac1b4fcfa50081350e6
bnocular <bnocular@gmail.com>
2020-12-05 02:05:04 +0700

    devops_sinau_01

---
+++ terraform/web/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+variable "name" {
+    description = "Name the instance on deploy"
+}
+
+resource "aws_instance" "devops_01_web" {
+    ami = "ami-00ddb0e5626798373"
+    instance_type = "t2.micro"
+    key_name = "devops_sinau"
+    
+    tags = {
+        Name = "${var.name}"
+    }
+}
